b06510281de79b55b8d543ed232b75c41dfb8db7
Lilinarvaez6 <31525470+Lilinarvaez6@users.noreply.github.com>
2017-12-03 03:42:45 -0500

    liliana-РC
    eliminar duplicados

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-
+//lo que se necesita 
 var express = require('express'); 
 var request = require('request'); 
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
+//lo de la aplicacion de Spotify
 var client_id = '956f9a50e95e4b888f59e45e27f6615e'; 
 var client_secret = '02d287a741f440d189d4a99a5064c379'; 
 var redirect_uri = 'http://localhost:8888/callback'; 
 
-
+//random de string
 var generateRandomString = function(length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -23,15 +24,19 @@ var stateKey = 'spotify_auth_state';
 
 var app = express();
 
+//direccion del contenido
 app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
 
+//Cuando de click en el boton de bienvenido  se acciona esta función 
 app.get('/login', function(req, res) {
 
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
 
-  var scope = 'user-read-private user-read-email';
+  //permisos necesarios para manipular y obtener información 
+  var scope = 'playlist-modify-public playlist-modify-private user-read-private user-read-email';
+  //cuando de click en el boton de bienvenido lo redirect al login de Spotify
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
@@ -39,15 +44,18 @@ app.get('/login', function(req, res) {
       scope: scope,
       redirect_uri: redirect_uri,
       state: state
+
+
     }));
 });
-
+//entra a la aplicacion
 app.get('/callback', function(req, res) {
 
   var code = req.query.code || null;
   var state = req.query.state || null;
   var storedState = req.cookies ? req.cookies[stateKey] : null;
 
+//verifica los estados (respuestas)
   if (state === null || state !== storedState) {
     res.redirect('/#' +
       querystring.stringify({
@@ -59,6 +67,7 @@ app.get('/callback', function(req, res) {
       url: 'https://accounts.spotify.com/api/token',
       form: {
         code: code,
+      
         redirect_uri: redirect_uri,
         grant_type: 'authorization_code'
       },
@@ -67,23 +76,28 @@ app.get('/callback', function(req, res) {
       },
       json: true
     };
-
+//lo bueno, como funciona
     request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
 
         var access_token = body.access_token,
             refresh_token = body.refresh_token;
 
+            //todo lo de la persona
         var options = {
           url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
+          headers: {'Authorization': 'Bearer ' + access_token },
+         
           json: true
         };
 
-        
+        //
         request.get(options, function(error, response, body) {
+          //mostrar en la consola de node
           console.log(body);
         });
+
+        
 
   
         res.redirect('/#' +
@@ -100,7 +114,7 @@ app.get('/callback', function(req, res) {
     });
   }
 });
-
+//ya no uso pero es lo de refresh 
 app.get('/refresh_token', function(req, res) {
 
 
